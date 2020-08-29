c2ca963728c869ee4356bbb2a5dcf0d7b10b1ac4
彭立璇 <kunbu0203@gmail.com>
2020-08-30 00:46:19 +0800

    新增banner

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            revEl: '.swiper-button-prev',
+        },
+    })
+
     $('#product').click(function (e) { 
         e.preventDefault();
         $('#subNav').slideToggle();
