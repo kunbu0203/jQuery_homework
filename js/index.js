1a218518a1071dd5476c94311b1d7741bfc3d1cf
彭立璇 <kunbu0203@gmail.com>
2020-08-30 16:07:46 +0800

    加上menu scroll

--- js/index.js
+++ js/index.js
@@ -10,12 +10,45 @@ $(document).ready(function () {
         navigation: {
             nextEl: '.swiper-button-next',
             revEl: '.swiper-button-prev',
+            
         },
     })
+
+    $('#index').click(function (e) { 
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: $('.banner').offset().top
+        }, 500);
+    });
 
     $('#product').click(function (e) { 
         e.preventDefault();
         $('#subNav').slideToggle();
         $(this).parent().toggleClass('active');
+    });
+
+    $('#intro').click(function (e) { 
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: $('.intro').offset().top - 50
+        }, 500);
+        $('#subNav').slideToggle();
+        $('#product').parent().removeClass('active');
+    });
+
+    $('#difference').click(function (e) { 
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: $('.difference').offset().top - 50
+        }, 500);
+        $('#subNav').slideToggle();
+        $('#product').parent().removeClass('active');
+    });
+
+    $('#contact').click(function (e) { 
+        e.preventDefault();
+        $('html').animate({
+            scrollTop: $('.contact').offset().top
+        }, 500);
     });
 });
